76207392ad447e447a409076ec14d54c40a0d651
juki3r <deepong25@gmail.com>
2024-02-13 12:35:32 +0800

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Mail\MyMailingServices;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
