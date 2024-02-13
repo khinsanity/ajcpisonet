959e90b0c8be4f7f0a0abfec673cc4aa43844c90
juki3r <deepong25@gmail.com>
2024-02-13 14:43:12 +0800

    modified:   resources/views/include/applynow.blade.php 	modified:   resources/views/include/contactus.blade.php

--- resources/views/include/applynow.blade.php
+++ resources/views/include/applynow.blade.php
@@ -1,5 +1,5 @@
 @extends('mainlayout')
-@section('title', 'AJC-InternetPlans')
+@section('title', 'AJC-Apply')
 @section('content')
     <h1>This is apply Page</h1>
 @endsection
--- resources/views/include/contactus.blade.php
+++ resources/views/include/contactus.blade.php
@@ -1,5 +1,5 @@
 @extends('mainlayout')
-@section('title', 'AJC-InternetPlans')
+@section('title', 'AJC-ContactUs')
 @section('content')
     <h1>This is Contact Page</h1>
 @endsection
