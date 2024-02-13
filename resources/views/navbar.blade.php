4b4b25377dca0cad57340d02fa968faf33443f41
juki3r <deepong25@gmail.com>
2024-02-13 13:08:37 +0800

    modified:   resources/views/navbar.blade.php

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -22,10 +22,6 @@
                         <a href="{{ url('/dashboard') }}" >Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" >Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" >Register</a>
-                        @endif
                     @endauth
                 </div>
             @endif
