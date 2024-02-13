d6b58f8218b4a39c274caed349f97da2a883ee79
juki3r <deepong25@gmail.com>
2024-02-13 15:02:37 +0800

    modified:   resources/views/components/primary-button.blade.php 	modified:   resources/views/layouts/guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div class="w-full sm:max-w-md mt-6 px-6 bg-white py-4 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-3 px-6 bg-white py-4 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
