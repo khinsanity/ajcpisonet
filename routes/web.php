b2d903460104e9b306cfb72e47970d8d28c4e580
juki3r <deepong25@gmail.com>
2024-02-13 20:04:06 +0800

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\ProfileController;
 */
 
 Route::get('/', function () {
-    return view('homepage');
+    return view('homepage')->name('homepage');
 });
 
 Route::get('/dashboard', function () {
