1562cba5ed48dbd04ae7da4f12906ad6b15aba3e
juki3r <deepong25@gmail.com>
2024-02-13 15:00:22 +0800

    modified:   resources/views/layouts/guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -3,12 +3,6 @@
 
 @section('content')
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-
             <div class="w-full sm:max-w-md mt-6 px-6 bg-white py-4 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
