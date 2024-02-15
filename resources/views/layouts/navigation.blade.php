a20d0ca5435289133534d7c4db6d9e3f8dd6ede1
juki3r <deepong25@gmail.com>
2024-02-15 21:37:32 +0800

    modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,7 +18,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Bill') }}
                     </x-nav-link>
                 </div>
             </div>
