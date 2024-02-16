d216e9cc5e87ac64f6a9ee8e8a5d41675dbc6283
juki3r <148548055+juki3r@users.noreply.github.com>
2024-02-16 15:53:39 +0800

    new

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->foreign('email')->references('email')->on('applications');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('type')->default(false); //add type boolean Users: 0=>User, 1=>Admin, 2=>Manager 
             $table->rememberToken();
             $table->timestamps();
         });
