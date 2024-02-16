6d8a473d056dd08d2a980360fafdb1b60b450be8
juki3r <deepong25@gmail.com>
2024-02-16 17:57:13 +0800

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -20,19 +20,10 @@ use App\Http\Controllers\DashboardController;
 */
 
 Route::get('/', [AJCController::class, 'homepage']);
-
-
-
+Route::get('/dashboard', [DashboardController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/admindashboard', [DashboardController::class, 'admindashboard'])->middleware(['auth', 'verified'])->name('admindashboard');
 Route::get('/addClient', [ClientsController::class, 'addClient'])->name('addClient');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
-    Route::get('/admindashboard', [DashboardController::class, 'admindashboard'])->name('admindashboard');
-    
-});
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/bill', function () {
     return view('bill');
 })->middleware(['auth', 'verified'])->name('bill');
