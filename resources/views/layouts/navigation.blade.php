6a9f4531a54c16a67ec76f6e0a9e2d5ebbcc286c
juki3r <deepong25@gmail.com>
2024-02-16 19:02:19 +0800

    modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,14 +12,13 @@
 
                 <!-- Navigation Links -->
               
-               
+                <!--Admin type -->
+                @if(Auth::user()->usertype=='admin')
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('admindashboard')" :active="request()->routeIs('admindashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
-                 <!--Admin type -->
-                @if(Auth::user()->usertype=='admin')
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('addClient')" :active="request()->routeIs('addClient')">
                         {{ __('AddClient') }}
@@ -27,6 +26,11 @@
                 </div>
                 @else
                 <!--User type -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Dashboard') }}
+                    </x-nav-link>
+                </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('bill')" :active="request()->routeIs('bill')">
                         {{ __('Bill') }}
