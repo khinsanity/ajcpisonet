5026c81b923e2e6e6a5411f6f9762f62461dd668
juki3r <deepong25@gmail.com>
2024-02-18 21:55:23 +0800

    new file:   app/Http/Controllers/AllclientController.php 	new file:   app/Models/Allclient.php 	new file:   database/migrations/2024_02_18_134729_create_allclients_table.php 	modified:   resources/views/admindashboard.blade.php 	modified:   app/Http/Controllers/AllclientController.php 	modified:   routes/web.php

---
+++ app/Models/Allclient.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Allclient extends Model
+{
+    use HasFactory;
+
+    protected $table = 'allclients';
+
+    protected $fillable = [
+        'fullname',
+        'address',
+        'plan',
+        'accountNumber'
+    ]
+}
