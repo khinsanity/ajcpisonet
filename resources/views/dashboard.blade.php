8e5430e50045c8ef5de7af1988fabf8c4b1699ee
juki3r <deepong25@gmail.com>
2024-02-18 21:25:03 +0800

    modified:   resources/views/dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                   {{Auth::user()->accountNumber}}
+                    @if(Auth::user()->accountNumber==0)
+                        <h1>Its zero</h1>
+                    @else
+                        <h1>It is not Zero</h1>
+                    @endif
                 </div>
             </div>
         </div>
