d8f2697e004f51fd7fc013442a51d0ff5886a07c
juki3r <deepong25@gmail.com>
2024-02-19 17:14:07 +0800

    modified:   resources/views/include/editclient.blade.php

--- resources/views/include/editclient.blade.php
+++ resources/views/include/editclient.blade.php
@@ -1,11 +1,51 @@
+<div class="modal" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>Modal body text goes here.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
-<x-app-layout>
-  <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Update Client') }}
-        </h2>
-    </x-slot>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{{-- 
     <div class="py-12">
         <div class="max-w-10xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -48,11 +88,5 @@
                             </div>
                         </form>
                     </div>
-                   </div>
+                   </div> --}}
                    
-
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout> 
