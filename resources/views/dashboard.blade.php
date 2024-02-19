cf5102f0e3a421a262e686ac3ee8414a3a24a5cd
juki3r <deepong25@gmail.com>
2024-02-19 22:05:59 +0800

    modified:   resources/views/dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,6 +16,7 @@
             <div class="row flex justify-content-center mt-5">              
                 <div class="col-4 p-3 border rounded shadow">
                     <h4 class="text-center">Please link your account</h4>
+                    <h5>{{$data}}</h5>
                     <form action="" method="POST">
                         @csrf
                         <div class="mb-3">
