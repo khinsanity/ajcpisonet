983a53e472383cc357f3038e1a6a6150c700231a
juki3r <deepong25@gmail.com>
2024-02-19 15:38:12 +0800

    modified:   resources/views/include/editclient.blade.php

--- resources/views/include/editclient.blade.php
+++ resources/views/include/editclient.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout>
-    <x-application-logo></x-application-logo>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Update Client') }}
