44c727d6835d67fd279e11d80e98af2f10ba20c0
juki3r <deepong25@gmail.com>
2024-02-19 17:32:12 +0800

    modified:   resources/views/dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,24 +5,27 @@
         </h2>
     </x-slot>
 
+    @if(Auth::user()->accountNumber==0)
+        <div class="container mt-3">
+            <div class="row flex justify-content-center">
+                hello
+            </div>
+        </div>
+
+
+    @else
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    @if(Auth::user()->accountNumber==0)
-                        <h4>Please Link Account</h4>
-                        <form action="" class="col-4">
-                            <div class="mb-2">
-                                <label>Account Number</label>
-                                <input type="text" class="form-control" />
-                                <button type="submit" class="btn btn-primary">Link</button>
-                            </div>
-                        </form>
-                    @else
+                    
+                      
+                    
                         <h1>It is not Zero</h1>
-                    @endif
+                    
                 </div>
             </div>
         </div>
     </div>
+    @endif
 </x-app-layout>
