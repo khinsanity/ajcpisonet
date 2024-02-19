30726585abda525a0f767acc539fadd6dbc156f0
juki3r <deepong25@gmail.com>
2024-02-19 15:18:26 +0800

    modified:   resources/views/include/editclient.blade.php 	modified:   resources/views/layouts/app.blade.php 	modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-1 px-1 sm:px-1 lg:px-1">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
