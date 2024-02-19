6ad1e448d778d8062b0b12aa70cb38c5093319b3
juki3r <deepong25@gmail.com>
2024-02-19 11:58:08 +0800

    modified:   resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="max-w-7xl mx-auto py-1 px-4 sm:px-1 lg:px-1">
                         {{ $header }}
                     </div>
                 </header>
