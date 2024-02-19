2b72862a5c7a10a965ac456cab48247771b87eb6
juki3r <deepong25@gmail.com>
2024-02-19 17:38:05 +0800

    modified:   resources/views/dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,33 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ 
+            @if(Auth::user()->accountNumber==0)
+                __('No Account Linked') 
+            @else
+                __('Dashboard') 
+                
+            @endif  }}
         </h2>
     </x-slot>
 
     @if(Auth::user()->accountNumber==0)
         <div class="container mt-3">
-            <div class="row flex justify-content-center">
-                hello
+            <div class="row flex justify-content-center mt-5">              
+                <div class="col-4">
+                    <h4 class="text-center">Please link your account</h4>
+                    <form action="">
+                        <div class="mb-3">
+                            <label>Enter your account number</label>
+                            <input type="text" name="accountNumber" class="form-control" />
+                        </div>
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-primary">Link</button>
+                        </div>
+                        
+                    </form>
+                </div>
+                
             </div>
         </div>
 
