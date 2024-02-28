08f44a848041c08f207ec0b4170f8f9cf5079f14
juki3r <deepong25@gmail.com>
2024-02-28 22:27:10 +0800

    modified:   app/Http/Controllers/DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -41,7 +41,9 @@ class DashboardController extends Controller
             $ConfirmLink = Auth::user()->update($user);
                 if($ConfirmLink){
                     return redirect('dashboard');
-                }
+                }else{
+                    return redirect()->back()->with('status', 'Invalid account number!');
+                } 
         }else{
             return redirect()->back()->with('status', 'Invalid account number!');
         }     
