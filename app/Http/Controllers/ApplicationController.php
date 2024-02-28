d6058e48140bff2355a06b996e1b1f5d8282c04f
juki3r <deepong25@gmail.com>
2024-02-29 07:41:13 +0800

    modified:   app/Http/Controllers/ApplicationController.php 	modified:   routes/web.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -71,9 +71,10 @@ class ApplicationController extends Controller
         return redirect(route('applynow'))->with("success", "Application submitted, Please wait for email, text or call");
     }
 
-    public function approveclient($firstname, $lastname){
+    public function approveclient($firstname, $lastname, $contact){
         echo $firstname;
         echo $lastname;
+        echo $contact;
         // return redirect('newapplication');
     }
 
