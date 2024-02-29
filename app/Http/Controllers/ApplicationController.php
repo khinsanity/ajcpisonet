76c19e9b5b055a06f4b8d0ae585e6f118437a686
juki3r <deepong25@gmail.com>
2024-02-29 08:13:56 +0800

    modified:   app/Http/Controllers/ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -73,7 +73,7 @@ class ApplicationController extends Controller
 
     public function approveclient($id,$firstname, $lastname, $email, $contact, $birthday, $gender, $plan, $street, $barangay, $town, $province){
         Allclient::create([
-            'fullname' => $firstname . $lastname,
+            'fullname' => $firstname .' '. $lastname,
             'address' => $street . $barangay . $town . $province,
             'plan' => $plan,
             'accountNumber' => 0
