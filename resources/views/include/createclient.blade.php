ede1a80e38832171ffad22660597472936f2b35f
juki3r <deepong25@gmail.com>
2024-03-02 15:26:11 +0800

    modified:   resources/views/include/createclient.blade.php 	modified:   resources/views/include/modifyclient.blade.php

--- resources/views/include/createclient.blade.php
+++ resources/views/include/createclient.blade.php
@@ -32,16 +32,6 @@
                                 @error('address') <span class="text-danger">{{$message}}</span>@enderror
                             </div>
                             <div class="mb-3">
-                                <label>Plan</label>
-                                <input type="text" class="form-control" name="plan" value="{{old('plan')}}" />
-                                @error('plan') <span class="text-danger">{{$message}}</span>@enderror
-                            </div>
-                            {{-- <div class="mb-3">
-                                <label>User_ID</label>
-                                <input type="text" class="form-control" name="id" value="{{old('id')}}" />
-                                @error('id') <span class="text-danger">{{$message}}</span>@enderror
-                            </div> --}}
-                            <div class="mb-3">
                                 <label>Account Number</label>
                                 <input type="text" class="form-control" name="accountNumber" value="{{old('accountNumber')}}" />
                                 @error('accountNumber') <span class="text-danger">{{$message}}</span>@enderror
