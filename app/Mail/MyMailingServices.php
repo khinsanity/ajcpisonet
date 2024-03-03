8f2523311e1dc04c880506e2e2a1937255b12736
juki3r <deepong25@gmail.com>
2024-03-03 11:52:23 +0800

    modified:   app/Mail/MyMailingServices.php 	modified:   routes/web.php

--- app/Mail/MyMailingServices.php
+++ app/Mail/MyMailingServices.php
@@ -38,8 +38,8 @@ class MyMailingServices extends Mailable
     {
         return new Content(
             view:('mail'),
-            with: ['fullname' => $this->fullname],
-            with: ['accountnumber' => $this->accountnumber]
+            with: ['fullname' => $this->fullname, 'accountnumber' => $this->accountnumber]
+
         );
     }
 
