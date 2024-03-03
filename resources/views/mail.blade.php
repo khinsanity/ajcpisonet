08a467f8fee0a865b7dde7f097eacacfef24d45d
juki3r <deepong25@gmail.com>
2024-03-03 13:00:22 +0800

    modified:   resources/views/mail.blade.php

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -2,10 +2,10 @@
 <img style="width: 500px" height="150px" src="{{ $message->embed(public_path().'/assets/ajcofclogo.jpg')}}">
 
 <h2>Hi <strong style="text-transform: capitalize">{{$fullname}}</strong>,</h2>
-<p class="my-2">Below is the summary of your monthly billing:</p>
-<p>Account Number : {{$accountnumber}}</p>
-<p>Acoount Name : {{$fullname}}</p>
-<p>Current Charges : 1500</p>
+<p class="mt-2">Below is the summary of your monthly billing:</p>
+<p class="m-0">Account Number : {{$accountnumber}}</p>
+<p class="m-0" style="text-transform: capitalize">Acoount Name : {{$fullname}}</p>
+<p class="m-0">Current Charges : 1500</p>
 
 
 
