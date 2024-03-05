de83393031c101a7ac51cb8d2d9b93549693b723
juki3r <deepong25@gmail.com>
2024-03-05 08:16:01 +0800

    modified:   resources/views/dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -94,10 +94,9 @@
                                                 <td style="border:none"></td>
                                             </tr>
                                             <tr style="border:transparent" >
-                                                <td style="border:none" class="text-nowrap">
+                                                
                                                     SUMMARY OF CHARGES AND CREDITS
-                                                </td>
-                                                <td style="border:none; width:50%"></td>
+                                            
                                             </tr>
 
                                             
