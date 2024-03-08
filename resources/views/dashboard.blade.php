42063d5e230414e1609bd8a279e61dc0785d101d
juki3r <deepong25@gmail.com>
2024-03-08 17:19:20 +0800

    modified:   resources/views/dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
         </h2>
     </x-slot>
 
-    @if (Auth::user()->accountnumber == null || Auth::user()->accountnumber != $accountnumber)
+    @if (Auth::user()->accountnumber == null)
         <div class="main">
             <div class="container mt-3">
                 <div class="row flex justify-content-center mt-5 ">
@@ -44,6 +44,37 @@
                 </div>
             </div>
         </div>
+    @elseif (Auth::user()->accountnumber != $accountnumber)
+    <div class="main">
+        <div class="container mt-3">
+            <div class="row flex justify-content-center mt-5 ">
+                <div class="col-12 col-sm-12  col-lg-8 ">
+                    <div class="container">
+                        <h3 style="width: 55%;" class="pt-5 texts">A Greater Life Together, at Home</h3>
+                        <p class="paragraph fs-sm-6 ">A Greater Life Together, at Home
+                            Get the whole family connected with the greatest deal.</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-4  p-3 border rounded shadow link my-auto py-auto">
+                    <h4 class="text-center">Please link your account</h4>
+                    <form action="{{ route('linkaccount') }}" method="POST" class="my-4">
+                        @csrf
+                        <div class="my-3">
+                            <label>Enter your account number:</label>
+                            <input type="text" name="accountnumber" class="form-control" required />
+                            <x-auth-session-status class="mb-4 text-danger" :status="session('status')" />
+                            @error('accountnumber')
+                                <span class="text-danger font-medium text-sm">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="my-3">
+                            <button type="submit" class="btn btn-primary">Link Now</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
         {{-- User account linked --}}
     @else
